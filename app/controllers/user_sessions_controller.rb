da98756862b3ce2252569bf65286f3a8332bbef5
masashi2610 <masashihappily@gmail.com>
2024-10-02 14:28:19 +0900

    ログインログアウト

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,20 @@
+class UserSessionsController < ApplicationController
+    skip_before_action :require_login, only: %i[new create]
+  
+    def new; end
+  
+    def create
+      @user = login(params[:email], params[:password])
+      if @user
+        redirect_to root_path, notice: t("user_sessions.create.success")
+      else
+        flash.now[:alert] = t("user_sessions.create.failure")
+        render :new, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      logout
+      redirect_to root_path, notice: t("user_sessions.destroy.success"), status: :see_other
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "tops#index"
   resources :users, only: %i[new create destroy]
+  resources :password_resets, only: %i[new create edit update]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
